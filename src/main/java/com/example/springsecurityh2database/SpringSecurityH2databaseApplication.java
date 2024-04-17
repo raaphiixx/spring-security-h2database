8e2ce1699100239f104cdf2a6641b455b8d6644f
Raphaa <raphaatstudy@gmail.com>
2024-04-17 13:34:41 +0100

    First commit, first configurations about the project

---
+++ src/main/java/com/example/springsecurityh2database/SpringSecurityH2databaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityh2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityH2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityH2databaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityh2database/SpringSecurityH2databaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityh2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityH2databaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
